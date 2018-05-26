a259f10a3ef7ee20274bfe2c5eafb3d114fad0c1
fsrocha-dev <fsrocha.dev@gmail.com>
2018-05-26 01:08:33 -0300

    Pequeno Refactoring no blog

--- app.js
+++ app.js
@@ -2,7 +2,15 @@ var http = require('http');
 
 var server = http.createServer(function(req, res){
     res.writeHead(200, {'Content-Type': 'text/html'});
-    res.write("<h1>MicroBlog</h1>");
+    if(req.url == '/'){
+        res.write("<h1>MicroBlog</h1>");
+    }else if(req.url == '/artigo/'){
+        res.write("<h1>Página dos artigos do blog</h1>");   
+    }else{
+        res.writeHead(404, {'Content-Type': 'text/heml'});
+        res.write("Erro: Página não econtrada");
+    }
+
     res.end();
 });
 
