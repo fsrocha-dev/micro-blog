26c07bb70f6e4f9c2e669e996c0b60ee8cc717cf
fsrocha-dev <fsrocha.dev@gmail.com>
2018-05-26 00:49:56 -0300

    Iniciando MicroBlog

---
+++ app.js
@@ -0,0 +1,9 @@
+var http = require('http');
+
+var server = http.createServer(function(req, res){
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.write("<h1>MicroBlog</h1>");
+    res.end();
+});
+
+server.listen(3000);
